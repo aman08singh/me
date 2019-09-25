d3d76e11eff0c7e16ab73fd9c10c41137b601955
aman08singh <aman.rajkumar.singh@gmail.com>
2019-09-25 19:55:55 +0530

    Updates

---
+++ precache-manifest.28d868670e7ba3dcf0bd5d2d2e3eb24e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0b99e7804b30222f1fc6464b8032dd",
+    "url": "/amansingh/index.html"
+  },
+  {
+    "revision": "38e75477cfc16979b146",
+    "url": "/amansingh/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "6639f6097647b5d31e11",
+    "url": "/amansingh/static/js/2.5cb0ae34.chunk.js"
+  },
+  {
+    "revision": "38e75477cfc16979b146",
+    "url": "/amansingh/static/js/main.4af98046.chunk.js"
+  },
+  {
+    "revision": "9b4f803ec2b851f7efe3",
+    "url": "/amansingh/static/js/runtime-main.c238509c.js"
+  }
+]);
